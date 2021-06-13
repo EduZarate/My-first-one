372b3a7ee82ba97bddd57d22c9cd7617a91f7de1
EduZarate <85833734+EduZarate@users.noreply.github.com>
2021-06-13 18:28:47 -0300

    Add files via upload

---
+++ Suma, resta, multiplicación y división de dos números.py
@@ -0,0 +1,10 @@
+""" Implementar un algoritmo que permita ingresar dos númerosenteros
+por teclado y luego muestre la suma, resta, multiplicación y división de
+ambos números."""
+
+a = int(input("Ingrese un número: "))
+b = int(input("Ingrese el otro: "))
+print ("El resultado de la suma es: ",a+b)
+print ("El resultado de la resta es: ", a-b)
+print ("El resultado de la multiplicación es: ", a*b)
+print ("El resultado de la división es: ", a//b)
