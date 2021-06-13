0013e2071ca45b469f7ed31023a2669d955c73ff
EduZarate <85833734+EduZarate@users.noreply.github.com>
2021-06-13 19:56:42 -0300

    Add files via upload

---
+++ Promedio entre tres notas.py
@@ -0,0 +1,7 @@
+""" Implementar  un  algoritmo  que permita  el  ingreso  de
+3  notas  de  un alumno y muestre por pantalla el promedio de las notas."""
+
+nota1 = float(input("Ingrese calificación: "))
+nota2 = float(input("Ingrese calificación: "))
+nota3 = float(input("Ingrese calificación: "))
+print ("El promedio es: ",(nota1 + nota2 + nota3)/3)
