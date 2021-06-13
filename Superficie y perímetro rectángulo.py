049ed10f4fd1ee692868e2029b653e13a2443ca8
EduZarate <85833734+EduZarate@users.noreply.github.com>
2021-06-13 19:30:01 -0300

    Add files via upload

---
+++ Superficie y perímetro rectángulo.py
@@ -0,0 +1,9 @@
+""" Implementar  un  algoritmo  que  permita  ingresar  las  longitudes
+de  un rectángulo y muestre por pantalla el perímetro y la superficie
+de la figura."""
+
+a = int(input("Ingrese la base del rectángulo: "))
+b = int(input("Ingrese la altura del rectángulo: "))
+print ("La superficie del rectángulo es: ",a*b)
+print ("El perímetro del rectángulo es: ", a*2 + b*2)
+
